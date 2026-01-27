e2aa4b1c437b81bc97ec88dbc15803994bb04691
Swaranjali Harkare <168217741+Swaraharkare@users.noreply.github.com>
2026-01-27 13:32:21 +0530

    Add files via upload

---
+++ DML/InsertData_Into_Department.sql
@@ -0,0 +1,12 @@
+USE Employee;
+
+--Insert data into table
+INSERT INTO department VALUES
+(1,'Finance',50000,3000),
+(2,'IT',40000,5000),
+(3,'Mechanical',20000,1500),
+(4,'Marketing',28000,2800),
+(5,'HR',45000,4000);
+
+--Select all data from department
+SELECT * FROM department;
---
+++ DML/InsertData_Into_Employee_info.sql
@@ -0,0 +1,35 @@
+USE Employee;
+
+--Insert data into table
+INSERT INTO employee_info VALUES
+(1,'Shrutika Harkare','Female',22,2);
+
+--Insert multiple columns
+INSERT INTO employee_info VALUES
+(2,'Swaranjali Harkare','Female',24,5),
+(3,'Subodh Harkare','Male',27,1),
+(4, 'Aarav Sharma', 'Male', 29,4),
+(5, 'Ishani Gupta', 'Female', 25,5),
+(6, 'Rohan Mehta', 'Male', 31,2),
+(7, 'Sanya Malhotra', 'Female', 27,3),
+(8, 'Aditya Verma', 'Male', 24,1),
+(9, 'Kavya Nair', 'Female', 23,5),
+(10, 'Vikram Singh', 'Male', 35,3),
+(11, 'Pallavi Pilare', 'Female', 26,2),
+(12, 'Rahul Dravid', 'Male', 40,4),
+(13, 'Meera Joshi', 'Female', 28,2),
+(14, 'Arjun Kapoor', 'Male', 32,3),
+(15, 'Tara Deshmukh', 'Female', 22,1),
+(16, 'Somanath Gavade', 'Male', 29,5),
+(17, 'Pooja Hegde', 'Female', 27,1),
+(18, 'Siddharth Rao', 'Male', 33,3),
+(19, 'Riya Sen', 'Female', 25,4),
+(20, 'Yash Vardhan', 'Male', 26,2),
+(21, 'Sneha Patil', 'Female', 30,5),
+(22, 'Varun Dhawan', 'Male', 28,3),
+(23, 'Kriti Sanon', 'Female', 24,4),
+(24, 'Armaan Malik', 'Male', 27,5),
+(25, 'Dia Mirza', 'Female', 34,1),
+(26, 'Kabir Khan', 'Male', 31,3),
+(27, 'Dhanashree Tejam', 'Female', 29,2),
+(28, 'Ishaan Khattar', 'Male', 23,5);
